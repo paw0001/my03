86ac2608da306850c2e10a9ad93be98f39c2a283
pashazem <p@p.ru>
2021-11-10 16:50:11 +0300

    007

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello From Kubernetes</h1><br>";
 echo "Server IP Address is: $ip_server", "<br><p>";
-echo "Made by <font color=blue>Pasha Zem";
+echo "Made by <font color=blue>Pasha Zem !!!";
 ?>
